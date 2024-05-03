75a407dee4e9e9507de0df5b00cabf9c7113cd36
Mike <allegrotech89@gmail.com>
2024-05-03 21:32:29 +0100

    boostrap added successfully

--- crmproject/settings.py
+++ crmproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z#3+))t27prxy4!kdyws$g9rrix*mu$o122$541c^(_w%mjril
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
--- website/views.py
+++ website/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+# Create your views here.\
+
+def login_user (request):
+    pass
+def logout_user(request):
+    pass
+
+def home(request):
+    return render(request, 'home.html', {})
